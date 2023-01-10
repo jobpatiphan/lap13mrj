204b1751e9f25252e11b03ced049b7120d84e1ee
mr.j <patiphan_klinhom@cmu.ac.th>
2023-01-10 15:09:35 +0700

    Completed!!!!

--- lab13_1.cpp
+++ lab13_1.cpp
@@ -1,9 +1,22 @@
 #include<iostream>
 using namespace std;
 
+int fibonacci(int);
+
 int main(){
     int x;
     cin >> x;
     cout << fibonacci(x);
     return 0; 
 }
+
+int fibonacci(int n){
+    if(n == 0 || n == 1){
+        return n;
+    }
+    else{
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+}
+
+
